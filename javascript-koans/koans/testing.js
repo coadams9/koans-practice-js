63373010471329aa49c57d92ed856455c1b86a05
Cory <coadams9@gmail.com>
2019-06-20 16:54:21 -0400

    working through what have we learned

--- javascript-koans/koans/testing.js
+++ javascript-koans/koans/testing.js
@@ -124,3 +124,31 @@ let mappedPlusTwo = original.map(num => num + 2)
 
 // ----------------------------------------------------------
 
+var products = [
+    { name: "Sonoma", ingredients: ["artichoke", "sundried tomatoes", "mushrooms"], containsNuts: false },
+    { name: "Pizza Primavera", ingredients: ["roma", "sundried tomatoes", "goats cheese", "rosemary"], containsNuts: false },
+    { name: "South Of The Border", ingredients: ["black beans", "jalapenos", "mushrooms"], containsNuts: false },
+    { name: "Blue Moon", ingredients: ["blue cheese", "garlic", "walnuts"], containsNuts: true },
+    { name: "Taste Of Athens", ingredients: ["spinach", "kalamata olives", "sesame seeds"], containsNuts: true }
+];
+
+var i, j, hasMushrooms, productsICanEat = [];
+
+// for (i = 0; i < products.length; i += 1) {
+//     if (products[i].containsNuts === false) {
+//         hasMushrooms = false;
+//         for (j = 0; j < products[i].ingredients.length; j += 1) {
+//             let testSubj = products[i].ingredients[j]
+//             if (testSubj === "mushrooms") {
+//                 hasMushrooms = true;
+//             }
+//         }
+//         if (!hasMushrooms) productsICanEat.push(products[i]);
+//     }
+// }
+
+let noNuts = products.filter(obj => obj.containsNuts === false)
+let okToEat = noNuts.filter(pizza => !pizza.ingredients.includes('mushrooms'))
+console.log(okToEat[0].name)
+
+// ----------------------------------------------------------
